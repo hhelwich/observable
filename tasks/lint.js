20cfb07f0d07d023c466ee9f6c5065ae7c568d9f
Hendrik Helwich <hendrik@helwich.de>
2015-04-17 16:15:49 +0200

    Also lint source and spec scripts

--- tasks/lint.js
+++ tasks/lint.js
@@ -5,11 +5,15 @@ import stylish from 'gulp-jscs-stylish';
 
 let noop = () => {};
 
-gulp.task('lintTasks', () => {
-  gulp.src('tasks/**/*.js')
-    .pipe(jshint())
-    .pipe(jscs({ configPath: 'tasks/.jscsrc' }))
-    .on('error', noop)
-    .pipe(stylish.combineWithHintResults())
-    .pipe(jshint.reporter('jshint-stylish'));
-});
+let createLintTask = (dir) => {
+  gulp.task(`lint-${dir}`, () => {
+    gulp.src(`${dir}/**/*.js`)
+      .pipe(jshint())
+      .pipe(jscs({ configPath: `${dir}/.jscsrc` }))
+      .on('error', noop)
+      .pipe(stylish.combineWithHintResults())
+      .pipe(jshint.reporter('jshint-stylish'));
+  });
+};
+
+['tasks', 'src', 'spec'].forEach(createLintTask);
