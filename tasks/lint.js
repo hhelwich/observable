e135c4b027c6cc101df065860243cda22b3f75dd
Hendrik Helwich <hendrik@helwich.de>
2015-04-17 15:32:35 +0200

    Add jscs linting

--- tasks/lint.js
+++ tasks/lint.js
@@ -1,6 +1,15 @@
 import gulp from 'gulp';
+import jscs from 'gulp-jscs';
 import jshint from 'gulp-jshint';
+import stylish from 'gulp-jscs-stylish';
+
+let noop = () => {};
 
 gulp.task('lintTasks', () => {
-  return gulp.src('./tasks/**/*.js').pipe(jshint()).pipe(jshint.reporter('default'));
+  gulp.src('tasks/**/*.js')
+    .pipe(jshint())
+    .pipe(jscs({ configPath: 'tasks/.jscsrc' }))
+    .on('error', noop)
+    .pipe(stylish.combineWithHintResults())
+    .pipe(jshint.reporter('jshint-stylish'));
 });
