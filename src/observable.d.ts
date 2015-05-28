c6711ae3be30bf600c8e204b0a05edd01e89a76d
Hendrik Helwich <hendrik@helwich.de>
2015-05-28 20:20:05 +0200

    Remove error type

--- src/observable.d.ts
+++ src/observable.d.ts
@@ -1,27 +1,23 @@
-interface Observable<V, E> {
-  map<W>(fn: (value: V) => W): Observable<W, E>;
-  mapError<F>(fn: (error: E) => F): Observable<V, F>;
-  filter(predicate: (value: V) => boolean): Observable<V, E>;
-  filterError(predicate: (value: E) => boolean): Observable<V, E>;
-  merge<W, F>(observable: Observable<W, F>): Observable<V|W, E|F>;
-  concat<W, F>(observable: Observable<W, F>): Observable<V|W, E|F>;
-  first(): Observable<V, E>;
-  last(): Observable<V, E>;
-  skip(count: number): Observable<V, E>;
+interface Observable<T> {
+  map<U>(fn: (value: T) => U): Observable<U>;
+  filter(predicate: (value: T) => boolean): Observable<T>;
+  merge<U>(observable: Observable<U>): Observable<T|U>;
+  concat<U>(observable: Observable<U>): Observable<T|U>;
+  first(): Observable<T>;
+  last(): Observable<T>;
+  skip(count: number): Observable<T>;
 }
 
 interface ObservableStatic {
-  <V, E>(creator: (
-      push: (value: V, delay?: number) => void,
-      pushError: (error: E, delay?: number) => void,
+  <T>(creator: (
+      push: (value: T, delay?: number) => void,
       end: (delay?: number) => void,
-      process: <W, F>(
-        observable: Observable<W, F>,
-        onValue: (value: W, valueIndex: number, index: number) => V|void,
-        onError: (error: F, errorIndex: number, index: number) => E|void,
+      observe: <U>(
+        observable: Observable<U>,
+        onValue: (value: U, index: number) => T|void,
         onEnd: (size: number) => void
       ) => void
-    ) => void|(() => void|(() => void))): Observable<V, E>;
+    ) => void|(() => void|(() => void))): Observable<T>;
 }
 
 declare var O: ObservableStatic;
