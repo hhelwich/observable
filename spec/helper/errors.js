fd05d4801b6bb1cfe312bbed51e9dc6e27c1c3ab
Hendrik Helwich <hendrik@helwich.de>
2015-07-26 14:22:51 +0200

    Use error observable instead of function

--- spec/helper/errors.js
+++ spec/helper/errors.js
@@ -1,14 +1,7 @@
-// All uncought errors will be collected here. Newer errors have higher indices.
-let _errors = [];
-
-// Register global handler to collect uncought errors.
-window.onerror = (msg, url, line, col, error) => {
-  _errors.push(error);
-};
-
-// Return all global errors since startup or after the last call.
-export default () => {
-  const errors = _errors;
-  _errors = [];
-  return errors;
-};
+// Never ending Observable of uncaught errors.
+export default O(push => {
+  // Register global handler to collect uncaught errors.
+  window.onerror = (msg, url, line, col, error) => {
+    push(error);
+  };
+});
