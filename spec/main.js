21a692a4959b28c050f02768ef2a41b5e861dd56
Hendrik Helwich <hendrik@helwich.de>
2015-08-02 16:49:19 +0200

    Organize specs

--- spec/main.js
+++ spec/main.js
@@ -1,19 +1,9 @@
-import './constructor.spec';
-import './fromArray.spec';
-import './filter.spec';
-import './once.spec';
-import './map.spec';
-import './take.spec';
+import './_helper/main';
 
-import './@map.spec';
-import './@filter.spec';
-import './@take.spec';
+import setTimeout from './setTimeout.spec';
+import whiteBox from './whiteBox/index.spec';
+import constructor from './constructor/index.spec';
 
-import './_toArray.spec';
-import './_copyArray.spec';
-import './_appendArray.spec';
-import './_compose.spec';
-import './_curry.spec';
-import './_curryObs.spec';
-import './_isFunc.spec';
-import './_isObservable.spec';
+setTimeout();
+whiteBox();
+constructor();
--- spec/setTimeout.spec.js
+++ spec/setTimeout.spec.js
@@ -1,19 +1,23 @@
-describe('setTimeout', () => {
+export default () => {
 
-  it('respects call order if delay time is equal', done => {
-    const count = 500;
-    let k = 0;
-    for (let i = 0; i < count; i += 1) {
-      (j => {
-        setTimeout(() => {
-          expect(j).toBe(k);
-          k += 1;
-          if (k === count) {
-            done();
-          }
-        }, 0);
-      })(i);
-    }
+  describe('Browser: setTimeout()', () => {
+
+    it('respects call order if delay time is equal', done => {
+      const count = 500;
+      let k = 0;
+      for (let i = 0; i < count; i += 1) {
+        (j => {
+          setTimeout(() => {
+            expect(j).toBe(k);
+            k += 1;
+            if (k === count) {
+              done();
+            }
+          }, 0);
+        })(i);
+      }
+    });
+
   });
 
-});
+};
